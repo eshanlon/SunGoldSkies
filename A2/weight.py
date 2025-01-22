8fcd234c26d0f7400f124ffe29664a8726d1c7ea
tahoodo <adriantejeda818@gmail.com>
2025-01-21 21:41:23 -0800

    commit

--- A2/weight.py
+++ A2/weight.py
@@ -1,1 +1,7 @@
 # Weight Approx
+import numpy as np
+import math
+def weight_estimation(Wcrew, Wpayload, Wempty, Wo, m_batt, batt_se, prop_eff, LD):
+    g = 32.17 #ft/s^2
+    R = prop_eff * np.exp(batt_se) * LD (m_batt / Wo)
+    Wo = (Wcrew + Wpayload) / (1 - (Wempty/Wo) - (R * g / (prop_eff * np.exp(batt_se) * LD)))
