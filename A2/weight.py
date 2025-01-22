3d8acddf52644a4237c7642f7c2a90f5fb94ff5c
tahoodo <adriantejeda818@gmail.com>
2025-01-21 22:19:03 -0800

    commit

--- A2/weight.py
+++ A2/weight.py
@@ -1,7 +1,17 @@
 # Weight Approx
 import numpy as np
 import math
-def weight_estimation(Wcrew, Wpayload, Wempty, Wo, m_batt, batt_se, prop_eff, LD):
+def weight_estimation(Wcrew, Wpayload, Wempty, Wo, m_batt, batt_se, prop_eff, LD, tolerance = 1e-6, max_iterations = 100):
     g = 32.17 #ft/s^2
-    R = prop_eff * np.exp(batt_se) * LD (m_batt / Wo)
-    Wo = (Wcrew + Wpayload) / (1 - (Wempty/Wo) - (R * g / (prop_eff * np.exp(batt_se) * LD)))
+    iteration = 0
+    while iteration < max_iterations:
+        R = prop_eff * np.exp(batt_se) * LD (m_batt / Wo)
+        New_Wo = (Wcrew + Wpayload) / (1 - (Wempty/Wo) - (R * g / (prop_eff * np.exp(batt_se) * LD)))
+
+        if abs(New_Wo - Wo) < tolerance:
+            return New_Wo
+        
+        Wo = New_Wo
+        iteration += 1
+
+    raise ValueError("Convergence not possible with current max_iterations.")
