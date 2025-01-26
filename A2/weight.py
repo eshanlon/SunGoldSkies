5f154d82ec09290167300eee7f41a93dad9f3da2
tahoodo <adriantejeda818@gmail.com>
2025-01-26 12:40:20 -0800

    commit

--- A2/weight.py
+++ A2/weight.py
@@ -1,17 +1,39 @@
 # Weight Approx
 import numpy as np
 import math
-def weight_estimation(Wcrew, Wpayload, Wempty, Wo, m_batt, batt_se, prop_eff, LD, tolerance = 1e-6, max_iterations = 100):
-    g = 32.17 #ft/s^2
-    iteration = 0
-    while iteration < max_iterations:
-        R = prop_eff * np.exp(batt_se) * LD (m_batt / Wo)
-        New_Wo = (Wcrew + Wpayload) / (1 - (Wempty/Wo) - (R * g / (prop_eff * np.exp(batt_se) * LD)))
-
-        if abs(New_Wo - Wo) < tolerance:
-            return New_Wo
-        
+def weight_estimation(Wcrew, Wpayload, Wo, m_batt, batt_se, prop_eff, LD):
+    g = 32.17 # ft/s^2
+    R = 417420 # ft
+    A = 0.74 # From table 2.1 metabook
+    C = -0.03 # From table 2.1 metabook
+    err = 1e-6
+    delta = 2*err
+    E01_EC = 1 # fill in from somewhere
+    E12_EC = 1 # fill in from somewhere
+    E23_EC = 1 # fill in from somewhere
+    E34_EC = 1 # fill in from somewhere
+    E45_EC = 1 # fill in from somewhere
+    while delta > err:
+        We_Wo = A * Wo ** C
+        m_batt = (R * Wo) / (prop_eff * batt_se * LD)
+        New_Wo = (Wcrew + Wpayload) / (1 - We_Wo - (((m_batt * g) / Wo) * (1 + E01_EC + E12_EC + E23_EC + E34_EC + E45_EC)))
+        delta = abs(New_Wo - Wo) / abs(New_Wo)
         Wo = New_Wo
-        iteration += 1
+    print(Wo)
 
-    raise ValueError("Convergence not possible with current max_iterations.")
+Wcrew = 180 * 32.17 # lbm * g = lbf
+Wpayload = 2000 * 32.17 # lbm * g = lbf
+Wo = 15000 * 32.17 # lbm * g = lbf
+batt_se = 722946.005 # ft-lbf/lbm
+prop_eff = 0.5
+LD = 10
+R = 417420 # ft
+m_batt = (R * Wo) / (prop_eff * batt_se * LD)
+print(m_batt)
+A = 0.74
+C = -0.03
+g = 32.17 #ft/s^2
+We_Wo = A * Wo ** C
+print(We_Wo)
+
+#weight_estimation(Wcrew, Wpayload, Wo, m_batt, batt_se, prop_eff, LD)
