ffcb19f2bc074e7911111d87e3ca6885af51f235
tahoodo <adriantejeda818@gmail.com>
2025-01-27 19:35:14 -0800

    commit

--- A2/weight.py
+++ A2/weight.py
@@ -54,11 +54,23 @@ R = 417420 # ft
 #g = 32.17 #ft/s^2
 #We_Wo = A * Wo ** C
 #print(We_Wo)
-
-iterationcount, convergedweight = weight_estimation(Wcrew, Wpayload, Wo, batt_se, prop_eff, LD)
-plt.plot(iterationcount, convergedweight, color="g", marker = "s", markersize=4, markerfacecolor="green")
-plt.title('Preliminary Estimation of Takeoff Weight (W\u2080)')
-plt.legend(loc='best')
-plt.xlabel('# of iterations')
-plt.ylabel('Preliminary Takeoff Weight [lbs]')
-plt.show()
+w1w0 = 0.995 #w3w1 Roskam ; Engine Start/Takeoff
+w2w1 = 0.998 #w4w3 Roskam ; Climb
+w4w3 = 0.999 #w7w5 Roskam ; Descent
+w5w4 = 0.998 #w8w7 Roskam ; Landing
+ #Cruise - Hydrocarbon Equivalent Fuel Fractions
+c = 0.02061944444 #[1/s]; Check notes for derivation
+v = 151.903
+w3w2 = math.exp(-((R * c)/(v * LD)))
+print(w3w2)
+m_batt = (R * Wo) / (prop_eff * batt_se * LD)
+g = 32.17 # ft/s^2
+test = - (((m_batt * g) / Wo) + (1-(w1w0)) + ((1-w2w1)*w1w0) + ((1-w4w3)*(w3w2*w2w1*w1w0)) + ((1-w5w4)*(w4w3*w3w2*w2w1*w1w0)))
+print(test)
+#iterationcount, convergedweight = weight_estimation(Wcrew, Wpayload, Wo, batt_se, prop_eff, LD)
+#plt.plot(iterationcount, convergedweight, color="g", marker = "s", markersize=4, markerfacecolor="green")
+#plt.title('Preliminary Estimation of Takeoff Weight (W\u2080)')
+#plt.legend(loc='best')
+#plt.xlabel('# of iterations')
+#plt.ylabel('Preliminary Takeoff Weight [lbs]')
+#plt.show()
