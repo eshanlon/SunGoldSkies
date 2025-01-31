d3373e634506b3de83ec9a58cf7682565e70ef02
tahoodo <adriantejeda818@gmail.com>
2025-01-30 17:13:52 -0800

    commit

--- A2/weight.py
+++ A2/weight.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def weight_estimation(Wcrew, Wpayload, Wo, batt_se, prop_eff, LD, max_interations = 20):
     iteration = 0
     g = 32.17 # ft/s^2
-    R = 100000 # ft
+    R = 425328 # ft
     A = 0.74 # From table 2.1 metabook
     C = -0.03 # From table 2.1 metabook
     err = 1e-6
@@ -13,23 +13,39 @@ def weight_estimation(Wcrew, Wpayload, Wo, batt_se, prop_eff, LD, max_interation
     convergedweight = []
     iterationcount = []
     v = 337.562 # ft/s
+    n_f = .83 # percent of power used for turboprop
+    n_b = 1 - n_f # percent of power used for battery
     while delta > err and iteration < max_interations:
         We_Wo = A * Wo ** C
         m_batt = (R * Wo) / (prop_eff * batt_se * LD)
         P_aircraft = v * (Wo / LD) # [ft-lbf/s]
         T = (P_aircraft) / (v) # [lbf]
-        c = 0.25 * (T / m_batt) * (1 / 3600) # [1/s]
+        cb = 0.25 * (T / m_batt) * (1 / 3600) # [1/s]
         W1 = .996 * Wo
         W2 = W1 * .998
-        W3 = W2 * math.exp(-((R * c) / (v * LD)))
+        W3 = W2 * math.exp(-((R * cb) / (v * LD)))
         W4 = W3 * .999
         W5 = W4 * .998
         E01_EC = (1 - (W1/Wo)) * (Wo / W3)
         E12_EC = (1 - (W2/W1)) * (W1 / W3)
-        E23_EC = 1 # b/c E_cruise/ E_cruise = 1
+        E23_EC = 0 #1 # b/c E_cruise/ E_cruise = 1
         E34_EC = (1 - (W4/W3)) * (W3 / W3)
         E45_EC = (1 - (W5/W4)) * (W4 / W3)
-        New_Wo = (Wcrew + Wpayload) / (1 - We_Wo - (((m_batt * g)/ Wo) * (1 + E01_EC + E12_EC + E23_EC + E34_EC + E45_EC)))
+        W5_Wo = W5 / Wo
+        while delta > err and iteration < max_interations:
+            Wf_Wo = 1 - W5_Wo
+            Wf = Wo * Wf_Wo
+            m_fuel = Wf / g
+            cf = 0.25 * (T / m_fuel) * (1 / 3600) # [1/s]
+            W1_Wo = .996
+            W2_W1 = .998
+            W3_W2 = math.exp(-((R * cf) / (v * LD)))
+            W4_W3 = .999
+            W5_W4 = .998
+            New_W5_Wo = W5_W4 * W4_W3 * W3_W2 * W2_W1 * W1_Wo
+            delta = abs(New_W5_Wo - W5_Wo) / abs(New_W5_Wo)
+            W5_Wo = New_W5_Wo
+        New_Wo = (Wcrew + Wpayload) / (1 - We_Wo - ((n_f * Wf_Wo) + (n_b * ((m_batt * g)/ Wo) * (1 + E01_EC + E12_EC + E23_EC + E34_EC + E45_EC))))
         delta = abs(New_Wo - Wo) / abs(New_Wo)
         Wo = New_Wo
         convergedweight.append(New_Wo / 32.17)
@@ -38,44 +54,12 @@ def weight_estimation(Wcrew, Wpayload, Wo, batt_se, prop_eff, LD, max_interation
         print(New_Wo/32.17)
     return iterationcount, convergedweight
 
-#R = 303805.7745 # ft
-#v = 337.562 # ft/s
-#g = 32.17 # ft/s^2
-#Wcrew = 180 * 32.17 # lbm * g = lbf
-#Wpayload = 2000 * 32.17 # lbm * g = lbf
-#Wo = 8000 * 32.17 # lbm * g = lbf
-#batt_se = 963928.0066 # ft-lbf/lbm
-#prop_eff = 0.9
-#LD = 50
-#A = 0.74 # From table 2.1 metabook
-#C = -0.03 # From table 2.1 metabook
-#We_Wo = A * Wo ** C
-#m_batt = (R * Wo) / (prop_eff * batt_se * LD)
-#P_aircraft = v * (Wo / LD)
-#T = (P_aircraft) / (v) # [lbf]
-#c = 0.25 * (T / m_batt) * (1 / 3600) # [1/s]
-#test = math.exp(-((R * c) / (v * LD)))
-#W1 = .996 * Wo
-#W2 = W1 * .998
-#W3 = W2 * math.exp(-((R * c) / (v * LD)))
-#W4 = W3 * .999
-#W5 = W4 * .998
-#E01_EC = (1 - (W1/Wo)) * (Wo / W3)
-#E12_EC = (1 - (W2/W1)) * (W1 / W3)
-#E23_EC = 1 # b/c E_cruise/ E_cruise = 1
-#E34_EC = (1 - (W4/W3)) * (W3 / W3)
-#E45_EC = (1 - (W5/W4)) * (W4 / W3)
-#New_Wo = (Wcrew + Wpayload) / (1 - We_Wo - (((m_batt * g)/ Wo) * (1 + E01_EC + E12_EC + E23_EC + E34_EC + E45_EC)))
-#test = - ((m_batt * g)/ Wo)
-#print(m_batt)
-#print(test)
-
 Wcrew = 180 * 32.17 # lbm * g = lbf
 Wpayload = 2000 * 32.17 # lbm * g = lbf
 Wo = 8000 * 32.17 # lbm * g = lbf
 batt_se = 1204910.008 # ft-lbf/lbm
-prop_eff = 0.9
-LD = 17
+prop_eff = 0.8
+LD = 10
 numiterations, converged_weight = weight_estimation(Wcrew, Wpayload, Wo, batt_se, prop_eff, LD)
 converged_weight = converged_weight
 plt.plot(numiterations, converged_weight, color="g", marker = "s", markersize=4, markerfacecolor="green")
