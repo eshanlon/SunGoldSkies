14a789adc1d23237e6eeb6e872bd091fde477ede
kayAero <kaycunningham@ucdavis.edu>
2025-01-31 16:25:02 -0800

    main_code updates

--- A2/A2_main_code.py
+++ A2/A2_main_code.py
@@ -1,6 +1,7 @@
 # Main Code for Assignment 2 Weight and Cost Estimations
 import cost
-
+import weight
+import matplotlib.pyplot as plt
 ############## Design 1: D&L ##########################
 cost.cost_analysis('A2/cost_estimate_DL.xlsx')
 
@@ -11,3 +12,40 @@ cost.cost_analysis('A2/cost_estimate_EK.xlsx')
 
 ############## Design 3: A&C ###########################
 cost.cost_analysis('A2/cost_estimate_AC.xlsx')
+
+############## Design 1: Weight Estimation #############
+Wcrew = 180 * 32.17 # lbm * g = lbf
+Wpayload = 2000 * 32.17 # lbm * g = lbf
+Wo = 9000 * 32.17 # lbm * g = lbf
+batt_se = 1204910.008 # ft-lbf/lbm
+batt_eff = 0.905
+LD = 7
+numiterations, converged_weight = weight.weight_estimation(Wcrew, Wpayload, Wo, batt_se, batt_eff, LD)
+plt.plot(numiterations, converged_weight, color="g", marker = "s", markersize=4, markerfacecolor="green")
+plt.title('Preliminary Estimation of Takeoff Weight (W\u2080)')
+plt.legend(loc='best')
+plt.xlabel('# of iterations')
+plt.ylabel('Preliminary Takeoff Weight [lbs]')
+plt.show()
+
+############## Design 2: Weight Estimation #############
+batt_eff = 0.905
+LD = 7
+numiterations, converged_weight = weight.weight_estimation(Wcrew, Wpayload, Wo, batt_se, batt_eff, LD)
+plt.plot(numiterations, converged_weight, color="g", marker = "s", markersize=4, markerfacecolor="green")
+plt.title('Preliminary Estimation of Takeoff Weight (W\u2080)')
+plt.legend(loc='best')
+plt.xlabel('# of iterations')
+plt.ylabel('Preliminary Takeoff Weight [lbs]')
+plt.show()
+
+############## Design 3: Weight Estimation #############
+batt_eff = 0.905
+LD = 8
+numiterations, converged_weight = weight.weight_estimation(Wcrew, Wpayload, Wo, batt_se, batt_eff, LD)
+plt.plot(numiterations, converged_weight, color="g", marker = "s", markersize=4, markerfacecolor="green")
+plt.title('Preliminary Estimation of Takeoff Weight (W\u2080)')
+plt.legend(loc='best')
+plt.xlabel('# of iterations')
+plt.ylabel('Preliminary Takeoff Weight [lbs]')
+plt.show()
