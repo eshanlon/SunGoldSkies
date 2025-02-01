2a5f27294881143b646459e3098452517e876bbf
kayAero <kaycunningham@ucdavis.edu>
2025-01-31 17:05:17 -0800

    updates

--- A2/A2_main_code.py
+++ A2/A2_main_code.py
@@ -1,6 +1,6 @@
 # Main Code for Assignment 2 Weight and Cost Estimations
 import cost
-import weight
+#import weight
 import matplotlib.pyplot as plt
 ############## Design 1: D&L ##########################
 cost.cost_analysis('A2/cost_estimate_DL.xlsx')
@@ -13,6 +13,7 @@ cost.cost_analysis('A2/cost_estimate_EK.xlsx')
 ############## Design 3: A&C ###########################
 cost.cost_analysis('A2/cost_estimate_AC.xlsx')
 
+'''
 ############## Design 1: Weight Estimation #############
 Wcrew = 180 * 32.17 # lbm * g = lbf
 Wpayload = 2000 * 32.17 # lbm * g = lbf
@@ -49,3 +50,6 @@ plt.legend(loc='best')
 plt.xlabel('# of iterations')
 plt.ylabel('Preliminary Takeoff Weight [lbs]')
 plt.show()
+'''
+
+print("Analysis Done!")
