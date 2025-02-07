f8d51e87c93dda5bfa52f8c44f788f54a8f4514a
tahoodo <adriantejeda818@gmail.com>
2025-02-07 15:19:13 -0800

    commit

--- A3/constraint_graph.py
+++ A3/constraint_graph.py
@@ -1,55 +1,79 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 #CDo = 0.03
 def stall_speed(rho, vstall, CLmax):
-    #rho = the density of desired alt
-    #v_stall = maximum stall speed set by the far requirment. Can also set stall speed lower
+    #rho = The density of desired alt
+    #v_stall = Maximum stall speed set by the far requirment. Can also set stall lower speed
     #CL_max = 1.3-1.9
     W_Sref = 1/2 * rho * (vstall**2) * CLmax
     return W_Sref
 
-def takeoff_distance(rho, CLmaxTO, BFL):
-    #rho = density of desired alt
-    #CL_maxTO = CL of choice
+def takeoff_distance(rho_rhoo, CLmaxTO, W_Sref, rho, prop_eff):
+    #rho_rhoo = Density ratio for alt
+    #CLmaxTO = CL of choice
     #BFL = ?
-    rhoSL = 0.002378 #slug/ft^3
-    TTO_WTO = []
+    #W_sref = Wing loading obtained 
+    #rho = Air density of choice
+    #prop_eff = Propeller effiency
+    WTO_Sref = .5 * rho * (vTO**2) *
+    TOP = WTO_Sref / (rho_rhoo * CLmaxTO * TTO_WTO)
+    T_W = []
     W_Sref = np.linspace(1, 300, 50)
-    TTO_WTO = W_Sref * (1 / ((rho/rhoSL) * CLmaxTO * (BFL / 37.5)))
-    return TTO_WTO
+    T_W = W_Sref * (1 / (rho_rhoo * CLmaxTO * (TOP * 37.5)))
+    v = math.sqrt((2 * W_Sref) / (rho * CLmaxTO))
+    W_P = prop_eff / (T_W * v)
+    return W_P
 
-def landingfield_length(rho, CLmaxL, Sa):
-    #rho = density of desired alt
-    #CL_maxL = CL of choice
+def landingfield_length(rho_rhoo, CLmaxL, Sa, BFL):
+    #rho = Density of desired alt
+    #CL_maxL = CL during landing
     #Sa = Braking distance
-    rhoSL = 0.002378 #slug/ft^3
-    SL = []
-    W_Sref = np.linspace(1, 300, 50)
-    SL = 80 * W_Sref * (1 / ((rho/rhoSL) * CLmaxL)) + Sa
-    return SL
+    #0.65 = Macimum landing to takeoff weight ratio(needs to be changed)
+    Sland = BFL * 0.6
+    W_S = rho_rhoo * CLmaxL * (Sland - Sa) / (80 * 0.65)
 
-def climb(ks, CLmaxCL, CDo, k, G):
-    #
-    #
-    #
-    #
-    #
-    T_W = []
-    T_W = ((ks**2) * CDo / CLmaxCL) + (CLmaxCL * k / (ks**2)) + G
-    return T_W
+    return W_S
 
-def cruise_speed(qcr, CDo, k):
-    #
-    #
-    #
+def climb(ks, CLmaxCL, CDo, e, AR, W_Sref, rho, prop_eff):
+    #ks = Speed to stall speed ratio
+    #CLmaxCL - CL during climb
+    #CDo = Minimum drag coefficent
+    #e = Wing efficieny ratio
+    #AR = Aspect Ratio
+    #W_sref = Wing loading obtained 
+    #rho = Air density of choice
+    #prop_eff = Propeller effiency
+    G = 0.083
+    k = 1 / (np.pi * e * AR)
     T_W = []
-    W_Sref = np.linspace(1, 300, 50)
+    T_W = (1 / 0.94)((ks**2) * CDo / CLmaxCL) + (CLmaxCL * k / (ks**2)) + G
+    v = math.sqrt((2 * W_Sref) / (rho * CLmaxCL))
+    W_P = prop_eff / (T_W * v)
+    return W_P
+
+def cruise_speed(v, CDo, e, AR, W_Sref, CLcruise, rho, prop_eff):
+    #v = Velocity during cruise
+    #CDo = Minimum drag coefficent
+    #e = Wing efficiency ratio
+    #AR = Aspect Ratio
+    #W_sref = Wing loading obtained 
+    #rho = Air density of choice
+    #prop_eff = Propeller effiency
+    #CLcruise = CL during cruise
+    qcr = 0.5 * rho * (v**2)
+    k = 1 / (np.pi * e * AR)
+    T_W = []
+    W_S = np.linspace(1, 300, 50)
     T_w = ((qcr * CDo) * (1 / W_Sref)) + ((k / qcr) * W_Sref)
-    return T_W
+    v = math.sqrt((2 * W_Sref) / (rho * CLcruise))
+    W_P = prop_eff / (T_W * v)
+    return W_P
 
-def absolute_ceiling(k, CDo):
-    #
-    #
+def absolute_ceiling(e, AR, CDo):
+    #e = Wing efficiency ratio
+    #AR = Aspect Ratio
+    #CDo = Minimum drag coefficent
     T_W = 2 * math.sqrt(k * CDo)
     return T_W
 
@@ -63,18 +87,24 @@ def sustained_turn(q, CDo, k, n):
     T_W = ((q * CDo) * (1 / W_Sref)) + (k(n**2/q)*W_Sref)
     return T_W
 
-def density(h, R):
+def density(h):
+    #h = height[m]
     #
     #
     #
     #
-    #
-    lamb = 6.5 #[K/km]
-    h = 1000 #[m]
+    lamb = 0.0065 #[K/m]
     To = 288.15 #[K]
     g = 9.81 #[m/s^2]
     R = 287 #[J/kg*K]
 
     rho_rhoo = (1 + ((lamb * h)/ To))**(-((g/ (R * lamb)) + 1))
-    return = rho_rhoo
-    
+    return rho_rhoo
+
+W_S = np.linspace(1, 500, 50)
+plt.plot(, W_S, color="g", marker = "s", markersize=1, markerfacecolor="green")
+plt.title('Preliminary Sizing')
+plt.legend(loc='best')
+plt.xlabel('W/S')
+plt.ylabel('W/P')
+plt.show()
