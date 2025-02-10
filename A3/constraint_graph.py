78506b904c2338366ead8c54d9a5edc0fda82658
tahoodo <adriantejeda818@gmail.com>
2025-02-09 23:13:44 -0800

    commit

--- A3/constraint_graph.py
+++ A3/constraint_graph.py
@@ -22,8 +22,11 @@ def stall_speed(rho, vstall, CLmax):
     #rho = The density of desired alt
     #v_stall = Maximum stall speed set by the far requirment. Can also set stall lower speed
     #CL_max = 1.3-1.9
-    W_Sref = 1/2 * rho * (vstall**2) * CLmax
-    return W_Sref
+    W_S = np.zeros(100)
+    for i in range(len(W_S)):
+        W_S[i] = 1/2 * rho * (vstall**2) * CLmax
+    W_P = np.linspace(1, 50, 100)
+    return W_S, W_P
 
 def takeoff_distance(rho_rhoo, vstall, CLmaxTO, rho, prop_eff):
     #rho_rhoo = Density ratio for alt
@@ -35,10 +38,10 @@ def takeoff_distance(rho_rhoo, vstall, CLmaxTO, rho, prop_eff):
     n = 100
     ks = 1.1 # estimation
     vTO = vstall * ks
-    P_W = .05 # From historical data
-    #TTO_WTO = (prop_eff / vTO) * P_W
+    P_W = .09 # From historical data
+    TTO_WTO = (prop_eff / vTO) * P_W
     WTO_Sref = .5 * rho * (vTO**2) * CLmaxTO
-    TOP = WTO_Sref / (rho_rhoo * CLmaxTO * P_W)
+    TOP = WTO_Sref / (rho_rhoo * CLmaxTO * TTO_WTO)
     BFL = TOP * 37.5
     T_W = np.zeros(n)
     v = np.zeros(n)
@@ -46,43 +49,33 @@ def takeoff_distance(rho_rhoo, vstall, CLmaxTO, rho, prop_eff):
     W_S = np.linspace(1, 50, n)
     for i in range(len(W_S)):
         T_W[i] = W_S[i] * (1 / (rho_rhoo * CLmaxTO * (TOP)))
-    print(T_W)
     for i in range(len(W_S)):
         v[i] = math.sqrt((2 * W_S[i]) / (rho * CLmaxTO))
         W_P[i] = prop_eff / (T_W[i] * v[i])
-    print(v)
-    print(W_P)
-    return W_P, T_W
+    return W_P, W_S
 
 rho_rhoo = density_ratio(1)
 
-W_P, T_W = takeoff_distance(rho_rhoo, vstall = 118.147, CLmaxTO = 1.4, rho = 0.002377, prop_eff = 0.7)
-W_P1, T_W1 = takeoff_distance(rho_rhoo, vstall = 118.147, CLmaxTO = 1.9, rho = 0.002377, prop_eff = 0.7)
-W_S = np.linspace(1, 50, 100)
-plt.plot(W_S, W_P, color="g", marker = "s", markersize=1, markerfacecolor="green")
-plt.plot(W_S, W_P1, color="r", marker = "s", markersize=1, markerfacecolor="red")
-plt.xlim(0,50)
-plt.ylim(0, 30)
-plt.title('Preliminary Sizing')
-plt.legend(loc='best')
-plt.xlabel('W/S')
-plt.ylabel('W/P')
-plt.show()
-
-def landingfield_length(rho_rhoo, CLmaxL, Sa, prop_eff, vL, rho):
+def landingfield_length(rho_rhoo, CLmaxL, Sa, prop_eff, vstall, rho):
     #rho = Density of desired alt
     #CL_maxL = CL during landing
     #Sa = Braking distance
-    #0.65 = Macimum landing to takeoff weight ratio(needs to be changed)
+    #0.65 = Macimum landing to takeoff weight ratio(needs to be changed
+    ks = 1.2
+    vL = ks * vstall
     P_W = 0.09 # From historical data
-    TTO_WTO = (prop_eff / vL) * P_W
+    TL_WL = (prop_eff / vL) * P_W
     WTO_Sref = .5 * rho * (vL**2) * CLmaxL
-    TOP = WTO_Sref / (rho_rhoo * CLmaxL * TTO_WTO)
+    TOP = WTO_Sref / (rho_rhoo * CLmaxL * TL_WL)
     BFL = TOP * 37.5
     Sland = BFL * 0.6
+    W_S = np.zeros(100)
     W_S = rho_rhoo * CLmaxL * (Sland - Sa) / (80 * 0.65)
-
-    return W_S
+    W_S = np.zeros(100)
+    for i in range(len(W_S)):
+        W_S[i] = 1/2 * rho * (vstall**2) * CLmaxL
+    W_P = np.linspace(1, 50, 100)
+    return W_S, W_P
 
 def climb(ks, CLmaxCL, CDo, e, AR, W_Sref, rho, prop_eff):
     #ks = Speed to stall speed ratio
@@ -113,13 +106,14 @@ def cruise_speed(v, CDo, e, AR, W_Sref, CLcruise, rho, prop_eff):
     qcr = 0.5 * rho * (v**2)
     k = 1 / (np.pi * e * AR)
     T_W = []
-    W_S = np.linspace(1, 300, 50)
+    W_S = np.linspace(1, 50, 100)
     T_w = ((qcr * CDo) * (1 / W_S)) + ((k / qcr) * W_S)
     v = math.sqrt((2 * W_Sref) / (rho * CLcruise))
     W_P = prop_eff / (T_W * v)
     return W_P
 
-def absolute_ceiling(e, AR, CDo):
+def absolute_ceiling(e, AR, CDo):]
+[\
     #e = Wing efficiency ratio
     #AR = Aspect Ratio
     #CDo = Minimum drag coefficent
@@ -137,3 +131,15 @@ def sustained_turn(q, CDo, k, n):
     T_W = ((q * CDo) * (1 / W_Sref)) + (k(n**2/q)*W_Sref)
     return T_W
 
+W_P, W_S = takeoff_distance(rho_rhoo, vstall = 118.147, CLmaxTO = 1.2, rho = 0.002377, prop_eff = 0.7)
+W_S1, W_P1 = stall_speed(rho = 0.002377, vstall = 118.147, CLmax = 1.3)
+print(W_S1)
+plt.plot(W_S, W_P, color="g", marker = "s", markersize=1, markerfacecolor="green")
+plt.plot(W_S1, W_P1, color="r", marker = "s", markersize=1, markerfacecolor="red")
+plt.xlim(10,80)
+plt.ylim(0, 60)
+plt.title('Preliminary Sizing')
+plt.legend(loc='best')
+plt.xlabel('W/S')
+plt.ylabel('W/P')
+plt.show()
