4e7956bd9ae75c115dfbc718f070171a5e888e90
kayAero <kaycunningham@ucdavis.edu>
2025-03-18 22:00:48 -0700

    update

--- PDR/cg_calculation.py
+++ PDR/cg_calculation.py
@@ -32,7 +32,7 @@ V_i = 50 #gal, integral tanks volume
 V_t = 200 #gal, total fuel volume
 W_dg = 13757 #lb, design gross weight
 W_en = 331 #lb, engine weight for each engine
-W_fw = 700 #lb, weight of fuel in wing
+W_fw = 850 #lb, weight of fuel in wing
 W_l = W_dg*0.03 #lb, landing design gross weight
 W_press = 0 #weight penalty due to pressurization, N/A for our aircraft
 W_uav = 1000 #lb, uninstalled avionics weight (typically 800-1400 lb)
@@ -46,13 +46,13 @@ W_ht = 0.016*(N_z*W_dg)**0.414*q**0.168*S_ht**0.896*(100*t_c/math.cos(lambda_s))
 # Vertical tail weight
 W_vt = 0.073*(1+0.2*Ht_Hv)*(N_z*W_dg)**0.376*q**0.122*S_vt**0.873*(100*t_c/math.cos(lambda_vt_s))**(-0.49)*(A/math.cos(lambda_vt_s)**2)**0.357*lambda_vt**0.039
 # Fuselage weight
-W_fuselage = 2*(0.052*S_f**1.086*(N_z*W_dg)**0.177*L_t**(-0.051)**(L/D)**(-0.072)*q**0.241+W_press) #the multiplying everything by 2 is a fudge factor
+W_fuselage = 2*(0.052*S_f**1.086*(N_z*W_dg)**0.177*L_t**(-0.051)**(L/D)**(-0.072)*q**0.241+W_press) 
 # Main landing gear weight
 W_mainlandinggear = 0.095*(N_l*W_l)**0.768*(L_m/12)**0.409
 # Nose landing gear weight (tail gear weight in our case)
 W_taillandinggear = 0.125*(N_l*W_l)**0.566*(L_n/12)**0.845
 # Installed engine weight
-W_installedengine = 2.575*W_en**0.922*N_en + W_en #added portion is for electric motor
+W_installedengine = 2.575*W_en**0.922*N_en + W_en 
 # Fuel system weight
 W_fuelsystem = 2.49*V_t**0.726*(1/(1+V_i/V_t))**0.363*N_l**0.242*N_en**0.157
 # Flight controls weight
@@ -66,7 +66,9 @@ W_electrical = 12.57*(W_fuelsystem+W_avionics)**0.51
 # Furnishings weight
 W_furnishings = 0.0582*W_dg-65
 # Battery Weight
-W_battery = 2*(304.36 *2.205) #kg to lbs
+W_battery = 705
+# Fuel Weight
+W_fuel = W_fw
 # Payload Weight
 W_payload = 2000 
 
@@ -85,5 +87,7 @@ print(f'Electrical Weight = {W_electrical}')
 print(f'Furnishings Weight = {W_furnishings}')
 print(f'Battery Weight = {W_battery}')
 print(f'Payload Weight = {W_payload}')
-total_weight = W_wing+W_ht+W_vt+W_fuselage+W_mainlandinggear+W_taillandinggear+W_installedengine+W_fuelsystem+W_flightcontrols+W_hydraulics+W_avionics+W_electrical+W_furnishings+W_battery+W_payload
-print(f'Total Weight = {total_weight}')
+takeoff_weight = W_wing+W_ht+W_vt+W_fuselage+W_mainlandinggear+W_taillandinggear+W_installedengine+W_fuelsystem+W_flightcontrols+W_hydraulics+W_avionics+W_electrical+W_furnishings+W_battery+W_payload+W_fuel
+print(f'Takeoff Weight = {takeoff_weight}')
+empty_weight = W_wing+W_ht+W_vt+W_fuselage+W_mainlandinggear+W_taillandinggear+W_installedengine+W_fuelsystem+W_flightcontrols+W_hydraulics+W_avionics+W_electrical+W_furnishings+W_battery
+print(f'Empty Weight = {empty_weight}')
